08ede37deff671237f3fd4ec66ea136d9fe04c8c
dengchong.999 <dengchong.999@bytedance.com>
2019-07-23 22:25:08 +0800

    androidstudio 导入so

---
+++ player_sdk/src/main/cpp/native-lib.cpp
@@ -0,0 +1,15 @@
+#include <jni.h>
+#include <string>
+
+extern "C"{
+#include "libavformat/avformat.h"
+}
+
+extern "C" JNIEXPORT jstring JNICALL
+Java_com_dengchong_mediaplayersdk_MainActivity_stringFromJNI(
+        JNIEnv *env,
+        jobject /* this */) {
+    av_register_all();
+    std::string hello = "Hello from C++";
+    return env->NewStringUTF(hello.c_str());
+}
