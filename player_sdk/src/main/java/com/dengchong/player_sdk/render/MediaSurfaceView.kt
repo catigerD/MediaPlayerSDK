8f6908ba07fd9344dd42db41958a58ff32558e65
dengchong.999 <dengchong.999@bytedance.com>
2019-08-02 13:29:18 +0800

    显示video成功

--- player_sdk/src/main/java/com/dengchong/player_sdk/render/MediaSurfaceView.kt
+++ player_sdk/src/main/java/com/dengchong/player_sdk/render/MediaSurfaceView.kt
@@ -1,15 +1,13 @@
 package com.dengchong.player_sdk.render
 
 import android.content.Context
-import android.graphics.BitmapFactory
 import android.opengl.GLES30
 import android.opengl.GLSurfaceView
-import android.opengl.GLUtils
-import android.os.Handler
-import android.os.Looper
 import android.util.AttributeSet
 import android.widget.Toast
 import com.dengchong.player_sdk.R
+import com.dengchong.player_sdk.utils.ALog
+import com.dengchong.player_sdk.utils.AThread
 import java.nio.ByteBuffer
 import java.nio.ByteOrder
 import java.nio.FloatBuffer
@@ -18,28 +16,39 @@ import javax.microedition.khronos.opengles.GL10
 
 class MediaSurfaceView(context: Context, attributeSet: AttributeSet?) : GLSurfaceView(context, attributeSet) {
 
+    private var renderer: MediaRender
+
     init {
         setEGLContextClientVersion(3)
-        setRenderer(MediaRender(context))
+        renderer = MediaRender(context)
+        setRenderer(renderer)
+        renderMode = RENDERMODE_WHEN_DIRTY
+    }
+
+    fun render(width: Int, height: Int, yData: ByteArray, uData: ByteArray, vData: ByteArray) {
+        renderer.setRenderParams(width, height, yData, uData, vData)
+        requestRender()
     }
 }
 
 class MediaRender(val context: Context) : GLSurfaceView.Renderer {
 
     private val vertexData = floatArrayOf(
-        -1f, -1f, 0f, 0f,
-        1f, -1f, 1f, 0f,
-        -1f, 1f, 0f, 1f,
-        1f, 1f, 1f, 1f
+        -1f, -1f, 0f, 1f,
+        1f, -1f, 1f, 1f,
+        -1f, 1f, 0f, 0f,
+        1f, 1f, 1f, 0f
     )
 
     private var vertexBuffre: FloatBuffer
     private var VBO = IntArray(1)
-    private var textures = IntArray(1)
+    private var textures = IntArray(3)
     private var program = 0;
     private var vertexPosition = 0
     private var texturePostion = 0
-    private var samplerTexture = 0
+    private var samplerYPosition = 0;
+    private var samplerUPosition = 0;
+    private var samplerVPosition = 0;
 
     init {
         vertexBuffre = ByteBuffer
@@ -51,33 +60,11 @@ class MediaRender(val context: Context) : GLSurfaceView.Renderer {
     }
 
     override fun onDrawFrame(gl: GL10?) {
-        if (program == 0) {
-            return
-        }
+
         GLES30.glClear(GLES30.GL_COLOR_BUFFER_BIT)
         GLES30.glClearColor(1.0f, 0.0f, 0.0f, 1.0f)
-
-        GLES30.glUseProgram(program)
-
-        GLES30.glBindBuffer(GLES30.GL_ARRAY_BUFFER, VBO[0])
-        GLES30.glEnableVertexAttribArray(vertexPosition)
-        GLES30.glVertexAttribPointer(vertexPosition, 2, GLES30.GL_FLOAT, false, 4 * 4, 0)
-        GLES30.glEnableVertexAttribArray(texturePostion)
-        GLES30.glVertexAttribPointer(texturePostion, 2, GLES30.GL_FLOAT, false, 4 * 4, 2 * 4)
-
-        GLES30.glActiveTexture(GLES30.GL_TEXTURE0)
-        GLES30.glBindTexture(GLES30.GL_TEXTURE_2D, textures[0])
-        val bitmap = BitmapFactory.decodeResource(context.resources, R.mipmap.ic_launcher)
-        if (bitmap != null) {
-            GLUtils.texImage2D(GLES30.GL_TEXTURE_2D, 0, bitmap, 0)
-            bitmap.recycle()
-        }
-        GLES30.glUniform1i(samplerTexture, 0)
-
+        render()
         GLES30.glDrawArrays(GLES30.GL_TRIANGLE_STRIP, 0, 4)
-
-        GLES30.glBindTexture(GLES30.GL_TEXTURE_2D, 0)
-        GLES30.glBindBuffer(GLES30.GL_ARRAY_BUFFER, 0)
     }
 
     override fun onSurfaceChanged(gl: GL10?, width: Int, height: Int) {
@@ -89,27 +76,98 @@ class MediaRender(val context: Context) : GLSurfaceView.Renderer {
         val fragmentSource = ShaderUtil.readRawTxt(context, R.raw.fragment_shader)
         program = ShaderUtil.createProgram(vertexSource, fragmentSource)
         if (program == 0) {
-            Handler(Looper.getMainLooper()).post {
+            AThread.runOnUiThread {
                 Toast.makeText(context, "program == 0", Toast.LENGTH_SHORT).show()
             }
             return;
         }
         vertexPosition = GLES30.glGetAttribLocation(program, "position")
         texturePostion = GLES30.glGetAttribLocation(program, "texture_coords")
-        samplerTexture = GLES30.glGetUniformLocation(program, "sTexture")
+        samplerYPosition = GLES30.glGetUniformLocation(program, "samplerY")
+        samplerUPosition = GLES30.glGetUniformLocation(program, "samplerU")
+        samplerVPosition = GLES30.glGetUniformLocation(program, "samplerV")
 
         GLES30.glGenBuffers(1, VBO, 0)
         GLES30.glBindBuffer(GLES30.GL_ARRAY_BUFFER, VBO[0])
         GLES30.glBufferData(GLES30.GL_ARRAY_BUFFER, vertexData.size * 4, vertexBuffre, GLES30.GL_STATIC_DRAW)
         GLES30.glBindBuffer(GLES30.GL_ARRAY_BUFFER, 0)
 
-        GLES30.glGenTextures(1, textures, 0)
+        GLES30.glGenTextures(3, textures, 0)
+        for (i in 0 until 3) {
+            GLES30.glBindTexture(GLES30.GL_TEXTURE_2D, textures[i])
+            GLES30.glTexParameteri(GLES30.GL_TEXTURE_2D, GLES30.GL_TEXTURE_WRAP_S, GLES30.GL_REPEAT)
+            GLES30.glTexParameteri(GLES30.GL_TEXTURE_2D, GLES30.GL_TEXTURE_WRAP_T, GLES30.GL_REPEAT)
+            GLES30.glTexParameteri(GLES30.GL_TEXTURE_2D, GLES30.GL_TEXTURE_MAG_FILTER, GLES30.GL_LINEAR)
+            GLES30.glTexParameteri(GLES30.GL_TEXTURE_2D, GLES30.GL_TEXTURE_MIN_FILTER, GLES30.GL_LINEAR)
+            GLES30.glBindTexture(GLES30.GL_TEXTURE_2D, 0)
+        }
+    }
+
+    private var width = 0;
+    private var height = 0;
+    private var yBuffer: ByteBuffer? = null
+    private var uBuffer: ByteBuffer? = null
+    private var vBuffer: ByteBuffer? = null
+
+    fun setRenderParams(width: Int, height: Int, yData: ByteArray, uData: ByteArray, vData: ByteArray) {
+        this.width = width
+        this.height = height
+        yBuffer = ByteBuffer.wrap(yData)
+        uBuffer = ByteBuffer.wrap(uData)
+        vBuffer = ByteBuffer.wrap(vData)
+    }
+
+    fun render() {
+        if (width == 0
+            || height == 0
+            || yBuffer == null
+            || uBuffer == null
+            || vBuffer == null
+
+        ) {
+            ALog.d("MediaRender render failed ...")
+        }
+
+        if (program == 0) {
+            return
+        }
+        GLES30.glUseProgram(program)
+
+        GLES30.glBindBuffer(GLES30.GL_ARRAY_BUFFER, VBO[0])
+        GLES30.glEnableVertexAttribArray(vertexPosition)
+        GLES30.glVertexAttribPointer(vertexPosition, 2, GLES30.GL_FLOAT, false, 4 * 4, 0)
+        GLES30.glEnableVertexAttribArray(texturePostion)
+        GLES30.glVertexAttribPointer(texturePostion, 2, GLES30.GL_FLOAT, false, 4 * 4, 2 * 4)
+
+        GLES30.glActiveTexture(GLES30.GL_TEXTURE0)
         GLES30.glBindTexture(GLES30.GL_TEXTURE_2D, textures[0])
-        GLES30.glTexParameteri(GLES30.GL_TEXTURE_2D, GLES30.GL_TEXTURE_WRAP_S, GLES30.GL_REPEAT)
-        GLES30.glTexParameteri(GLES30.GL_TEXTURE_2D, GLES30.GL_TEXTURE_WRAP_T, GLES30.GL_REPEAT)
-        GLES30.glTexParameteri(GLES30.GL_TEXTURE_2D, GLES30.GL_TEXTURE_MAG_FILTER, GLES30.GL_LINEAR)
-        GLES30.glTexParameteri(GLES30.GL_TEXTURE_2D, GLES30.GL_TEXTURE_MIN_FILTER, GLES30.GL_LINEAR)
-        GLES30.glBindTexture(GLES30.GL_TEXTURE_2D, 0)
+        GLES30.glTexImage2D(
+            GLES30.GL_TEXTURE_2D, 0, GLES30.GL_LUMINANCE, width, height, 0, GLES30.GL_LUMINANCE,
+            GLES30.GL_UNSIGNED_BYTE, yBuffer
+        )
+        GLES30.glActiveTexture(GLES30.GL_TEXTURE1)
+        GLES30.glBindTexture(GLES30.GL_TEXTURE_2D, textures[1])
+        GLES30.glTexImage2D(
+            GLES30.GL_TEXTURE_2D, 0, GLES30.GL_LUMINANCE, width / 2, height / 2, 0, GLES30.GL_LUMINANCE,
+            GLES30.GL_UNSIGNED_BYTE, uBuffer
+        )
+        GLES30.glActiveTexture(GLES30.GL_TEXTURE2)
+        GLES30.glBindTexture(GLES30.GL_TEXTURE_2D, textures[2])
+        GLES30.glTexImage2D(
+            GLES30.GL_TEXTURE_2D, 0, GLES30.GL_LUMINANCE, width / 2, height / 2, 0, GLES30.GL_LUMINANCE,
+            GLES30.GL_UNSIGNED_BYTE, vBuffer
+        )
+
+        GLES30.glUniform1i(samplerYPosition, 0)
+        GLES30.glUniform1i(samplerUPosition, 1)
+        GLES30.glUniform1i(samplerVPosition, 2)
+
+        yBuffer?.clear()
+        uBuffer?.clear()
+        vBuffer?.clear()
+        yBuffer = null
+        uBuffer = null
+        vBuffer = null
     }
 
 }
--- player_sdk/src/main/res/raw/fragment_shader.glsl
+++ player_sdk/src/main/res/raw/fragment_shader.glsl
@@ -1,6 +1,17 @@
 precision mediump float;
 varying vec2 out_texture_coords;
-uniform sampler2D sTexture;
+uniform sampler2D samplerY;
+uniform sampler2D samplerU;
+uniform sampler2D samplerV;
 void main(){
-    gl_FragColor = texture2D(sTexture, out_texture_coords);
+    float y, u, v;
+    y = texture2D(samplerY, out_texture_coords).r;
+    u = texture2D(samplerU, out_texture_coords).r - 0.5;
+    v = texture2D(samplerV, out_texture_coords).r - 0.5;
+
+    vec3 rgb;
+    rgb.r = y + 1.403 * v;
+    rgb.g = y - 0.344 * u - 0.714 * v;
+    rgb.b = y + 1.770 * u;
+    gl_FragColor = vec4(rgb, 1);
 }
